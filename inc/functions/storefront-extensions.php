f99d3471f203e4ab2157801454e25ac3474ee78a
James Koster <james@jameskoster.co.uk>
2015-01-14 10:51:46 +0000

    extension integrations

--- inc/functions/storefront-extensions.php
+++ inc/functions/storefront-extensions.php
@@ -10,9 +10,6 @@
  * @return  void
  */
 function wootique_extension_support() {
-	add_filter( 'storefront_blog_customiser_enabled', '__return_false' );
-	add_filter( 'storefront_checkout_customizer_enabled', '__return_false' );
 	add_filter( 'storefront_designer_enabled', '__return_false' );
-	add_filter( 'storefront_woocommerce_customiser_enabled', '__return_false' );
 }
 add_action( 'init', 'wootique_extension_support', -1 );
