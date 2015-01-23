446b9a8b26a8d75b33ae4a60d398b1ef8c814a98
James Koster <james@jameskoster.co.uk>
2015-01-23 11:11:08 +0000

    remove homepage settings from WooCommerce customiser. closes #12

--- inc/functions/storefront-extensions.php
+++ inc/functions/storefront-extensions.php
@@ -13,3 +13,14 @@ function boutique_extension_support() {
 	add_filter( 'storefront_designer_enabled', '__return_false' );
 }
 add_action( 'init', 'boutique_extension_support', -1 );
+
+/**
+ * Customize Storefront extension Customizer controls etc.
+ * @return void
+ */
+if ( ! function_exists( 'boutique_customize_register' ) ) {
+	function boutique_customize_register( $wp_customize ) {
+		$wp_customize->remove_section( 'storefront_homepage' );
+	}
+}
+add_action( 'customize_register', 'boutique_customize_register', 99 );
